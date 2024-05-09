5aa083d9e97d371fd58889fe2b0749e6d6cd97ac
David Campbell <tryggus20@gmail.com>
2024-05-09 15:56:39 -0500

    added validators and exceptions

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -5,6 +5,9 @@ import re
 from dataclasses import dataclass
 from functools import cached_property
 
+from tic_tac_toe.logic.validators import validate_game_state, validate_grid
+
+
 WINNING_PATTERNS = (
     "???......",
     "...???...",
@@ -29,8 +32,7 @@ class Grid:
     cells: str = " " * 9
 
     def __post_init__(self) -> None:
-        if not re.match(r"^[\sXO]{9}$", self.cells):
-            raise ValueError("Must contain 9 cells of: X, O, or space")
+         validate_grid(self)
 
     @cached_property
     def x_count(self) -> int:
@@ -56,6 +58,9 @@ class Move:
 class GameState:
     grid: Grid
     starting_mark: Mark = Mark("X")
+
+    def __post_init__(self) -> None:
+        validate_game_state(self)
 
     @cached_property
     def current_mark(self) -> Mark:
