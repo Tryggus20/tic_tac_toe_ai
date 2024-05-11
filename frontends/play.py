b221d71484faaafad8d88395c52ca2190687fb3f
David Campbell <tryggus20@gmail.com>
2024-05-11 14:32:26 -0500

    minmax is working

--- frontends/play.py
+++ frontends/play.py
@@ -9,3 +9,10 @@ player1 = ConsolePlayer(Mark("X"))
 player2 = RandomComputerPlayer(Mark("O"))
 
 TicTacToe(player1, player2, ConsoleRenderer()).play()
+
+"""  to play the game, run the program and type the location for your piece
+There are 3 types of players: 1-human, 2-random, 3-minimax
+to change the settings: 
+cd frontends
+python -m console -X minimax -0 minimax
+change the X and 0 to the type of player you would like and have fun!  """
