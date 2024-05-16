42192d29f27dee3be1dd1bf0ff7605dc7ed06387
David Campbell <tryggus20@gmail.com>
2024-05-16 10:04:30 -0500

    added tutorial link

--- frontends/play.py
+++ frontends/play.py
@@ -2,9 +2,12 @@
 There are 3 types of players: 1-human, 2-random, 3-minimax
 to change the settings, run: 
 cd frontends
-python -m console -X minimax -0 minimax
+python -m console -X minimax -O minimax
 change the X and 0 to the type of player you would like and have fun! 
-ps: manual entry moves accepts both a1 and 1a formats  """
+ps: manual entry moves accepts both a1 and 1a formats"""
+
+# tutorial I followed:
+# https://realpython.com/tic-tac-toe-ai-python/#step-2-scaffold-a-generic-tic-tac-toe-game-engine
 
 from tic_tac_toe.game.engine import TicTacToe
 from tic_tac_toe.game.players import RandomComputerPlayer
